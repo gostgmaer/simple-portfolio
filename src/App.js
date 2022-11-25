2ca5aba7f54628809ce24902630946936e68fa65
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-11-25 16:27:25 +0530

    router updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Router, Routes } from 'react-router-dom';
+import Home from './Routes/Home';
+import About from './Routes/About';
+import Contact from './Routes/Contact';
+import Projects from './Routes/Projects';
+import Project from './Routes/Project';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<React.Fragment>
+  <Routes>
+    <Route path='/' element={<Home></Home>} ></Route>
+    <Route path='/about' element={<About></About>} ></Route>
+    <Route path='/contact' element={<Contact></Contact>} ></Route>
+    <Route path='/projects' element={<Projects></Projects>} ></Route>
+     <Route path='/project/:id' element={<Project></Project>} ></Route>
+    
+  </Routes>
+</React.Fragment>
   );
 }
 
