8b488974a112818204217b8baff86e3c9026bc51
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-01-03 21:52:43 +0530

    Update Portfolio Details

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-import './intro.scss'
-const Intro = () => {
-  return (
-    <div className='Intro' id='intro'></div>
-  )
-}
+import React, { useEffect, useRef } from "react";
+import { MdArrowDownward, MdKeyboardArrowDown } from "react-icons/md";
+import { Mock } from "../../Assets/Data/Mock/Data";
+import "./intro.scss";
+import { init } from "ityped";
 
-export default Intro
+
+const Intro = () => {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      strings: ["Web", "Frontend", "Fullstack"],
+    });
+  }, []);
+
+ 
+
+  return (
+    <div className="Intro" id="intro">
+      <div className="left">
+        <div className="imageContainer">
+          <img src="./assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there i'm</h2>
+          <h1>Kishor Sarkar</h1>
+          <h4>
+            Freelance <span ref={textRef}></span>
+          </h4>
+          <a href="#portfolio">
+            <MdKeyboardArrowDown></MdKeyboardArrowDown>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
